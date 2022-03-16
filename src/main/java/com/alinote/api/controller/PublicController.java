e9c2694e1e25cb99cb4f60943e203f47298549a4
piyush170889 <piyushjadhav65@gmail.com>
2022-03-16 11:27:17 +0530

    Update PublicController.java

--- src/main/java/com/alinote/api/controller/PublicController.java
+++ src/main/java/com/alinote/api/controller/PublicController.java
@@ -30,7 +30,7 @@ public class PublicController {
     @GetMapping(value = {RestUrlConstants.PING})
     public String ping() {
         log.info("Pinged Successfully");
-        return "Ping successful!!-deployed. updated (Checked Piyush)";
+        return "Ping successful!!-deployed. Working";
     }
 
     @ApiOperation(value = "Api to register User")
